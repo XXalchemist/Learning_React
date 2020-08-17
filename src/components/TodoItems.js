695f037b9836d1c49f9f1a77ccc735a19ba6b7d7
Narayan Krishna <nkrishn8@gmail.com>
2020-08-17 11:39:30 +0530

    showing data from TodoItems

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class TodoItems extends Component {
     render() { 
         return (
-            <div>TodoItems</div>
+            <div>
+                <h3>{ this.props.todo.title }</h3>
+            </div>
         );
     }
 }
