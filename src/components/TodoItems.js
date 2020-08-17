0c886f9dca6275f807f424d2727e28ec44cd0573
Narayan Krishna <nkrishn8@gmail.com>
2020-08-17 11:32:52 +0530

    added TodoItem boiler

---
+++ src/components/TodoItems.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TodoItems extends Component {
+    render() { 
+        return (
+            <div>TodoItems</div>
+        );
+    }
+}
+ 
+export default TodoItems;
