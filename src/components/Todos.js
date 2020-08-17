029974eb76f40d71e5e8eacdc23f99bb7feeddc1
Narayan Krishna <nkrishn8@gmail.com>
2020-08-17 11:23:20 +0530

    todos component showing state data

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,11 +3,13 @@ import React, {Component} from 'react';
 class Todos extends Component {
     
     render(){
-    return (
+    return this.props.todos.map((todo)=>(
     <div className="Todo">
-    <h1>Todos Here</h1>
+    <h1>ID : { todo.id }</h1>
+    <h1>Title : { todo.title }</h1>
+    <h1>Completed : { todo.completed }</h1>
     </div>
-  )};
+  ))};
 }
 
 export default Todos;
