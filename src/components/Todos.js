3dd1e50198e1b47547b210b33687b7072fa0fe14
Narayan Krishna <nkrishn8@gmail.com>
2020-08-17 11:04:26 +0530

    created boiler for Todo component

---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Todo extends Component {
+    
+    render(){
+    return (
+    <div className="Todo">
+    <h1>Todos Here</h1>
+    </div>
+  )};
+}
+
+export default Todo;
