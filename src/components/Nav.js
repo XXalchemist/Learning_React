09fe2ce1a1ad6d663e7f48db5b8d197054c3d99b
Narayan Krishna <nkrishn8@gmail.com>
2020-08-17 13:00:40 +0530

    added navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function nav(){
+    return (
+       
+    <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+            <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+            Todo : Declutter Your Mind
+        </a>
+    </nav>
+
+    )
+}
+
+export default nav;
